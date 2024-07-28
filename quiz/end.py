f6ebf6501766873ca45eb05d3ad2c46e774c5215
Ong Chuan Kai <85627680+ong-ck@users.noreply.github.com>
2024-07-29 02:09:55 +0800

    Update end.py

--- quiz/end.py
+++ quiz/end.py
@@ -1,7 +1,51 @@
 import streamlit as st
+import base64
+from PIL import Image
 
+bg_img_path = "assets/math-curriculum.jpg.webp"
 
+# https://www.youtube.com/watch?v=pyWqw5yCNdo
+@st.cache_data
+def get_img_as_base64_with_opacity(file, opacity):
+    # Open the image file
+    with Image.open(file) as img:
+        # Ensure image has an alpha channel
+        img = img.convert("RGBA")
+
+        # Modify the alpha channel
+        alpha = img.split()[3]
+        alpha = alpha.point(lambda p: p * opacity)
+
+        # Merge the image with the new alpha channel
+        img.putalpha(alpha)
+
+        # Save the modified image to a bytes buffer
+        from io import BytesIO
+        buffer = BytesIO()
+        img.save(buffer, format="PNG")
+        buffer.seek(0)
+
+        # Encode the bytes buffer to base64
+        data = buffer.read()
+        return base64.b64encode(data).decode()
+        
 def run():
+    ### Add background image #################################
+    img = get_img_as_base64_with_opacity(bg_img_path, 0.2)
+    page_bg_img = f"""
+    <style>
+    [data-testid="stAppViewContainer"] {{
+        background-image: url('data:image/png;base64,{img}');
+        background-size: cover;
+    }}
+
+    [data-testid="stHeader"] {{
+    background-color: rgba(0,0,0,0);
+    }}
+    </style>
+    """
+    st.markdown(page_bg_img ,unsafe_allow_html=True)
+    ##################################
     if st.session_state.clear_quiz:
         st.title("Congratulations!")
         st.write("You have finished the quiz!")
@@ -12,7 +56,10 @@ def run():
 
     if st.button("Try again"):
         # Clear session state
-        st.session_state.clear()
+        # Except for user so that the user is not logged out
+        for key in st.session_state:
+            if key != "user":
+                del st.session_state[key]
 
         # Navigate back to the start page
         st.session_state.navigate = "start"
